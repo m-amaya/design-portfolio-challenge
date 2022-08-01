5142c78e9a7e17994ea88e6f8c3e28f0ae4dcdf0
Marissa Amaya <mamaya.dev@gmail.com>
2022-08-01 16:45:53 -0500

    init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import { globalStyles } from "design-portfolio-ds";
+import { FC } from "react";
+
+const HomePage: FC = () => {
+  globalStyles();
+
+  return <div>Hello, World!</div>;
+};
+
+export default HomePage;
